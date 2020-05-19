e3343c50fb070b17f10011d6a20fe749566be063
Zachary <zachjoliver@gmail.com>
2020-05-19 14:41:42 -0700

    Add initial candies file

---
+++ candies.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function (candies, extraCandies) {
+  for (let z = 0; z < candies.length; z++) {
+
+  }
+};
+
+//iterate through candies array, find the max
+//map through the candies array
+//current element is true if the elements value + extraCandies is >= max
