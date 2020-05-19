da0291229149f4cdb9ab72f5822759f6eb7f0fe2
Zachary <zachjoliver@gmail.com>
2020-05-19 14:49:08 -0700

    Initial solution passing for candies

--- candies.js
+++ candies.js
@@ -4,11 +4,14 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function (candies, extraCandies) {
-  for (let z = 0; z < candies.length; z++) {
-
-  }
+  let max = 0;
+  for (let z = 0; z < candies.length; z++) { if (candies[z] > max) max = candies[z] }
+  candies.map(candy => candy + extraCandies >= max)
+  return candies;
 };
 
+
+kidsWithCandies([2, 3, 5, 1, 3], 3);
 //iterate through candies array, find the max
 //map through the candies array
 //current element is true if the elements value + extraCandies is >= max
