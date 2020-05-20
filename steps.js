1a3837efba866597820cd8b4ad3585ccfcd438b2
Zachary <zachjoliver@gmail.com>
2020-05-19 21:19:01 -0700

    Final steps to reduce to zero function

--- steps.js
+++ steps.js
@@ -1,4 +1,6 @@
-var numberOfSteps = function (num) {
-  if (num < 3) return 1;
-  return num % 2 === 0 ? 1 + numberOfSteps(num - 1) : 1 + numberOfSteps(num / 2);
-};
+  var numberOfSteps = function (num) {
+    if (num < 2) return 1;
+    return num % 2 === 0 ? 1 + numberOfSteps(num / 2) : 1 + numberOfSteps(num - 1);
+  };
+
+numberOfSteps(123);
