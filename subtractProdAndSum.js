ed89105fd83ca501d3d757d8c86d4c866c2bebdd
Zachary <zachjoliver@gmail.com>
2020-05-23 11:13:56 -0700

    One more subtract prod and sum push

--- subtractProdAndSum.js
+++ subtractProdAndSum.js
@@ -1,11 +1,11 @@
 var subtractProductAndSum = function (n) {
   const nStr = n.toString();
-  let prod = 1;
-  let sum = 0;
-  for (let i=0;i<nStr.length;i++) {
-    prod *= parseInt(nStr[i]);
-    sum += parseInt(nStr[i]);
-  }
-  return prod - sum;
+  return nStr.split('').reduce((a,b) => parseInt(a) * parseInt(b)) -  nStr.split('').reduce((a, b) => parseInt(a) + parseInt(b));
 };
+
+  // for (let i=0;i<nStr.length;i++) {
+  //   prod *= parseInt(nStr[i]);
+  //   sum += parseInt(nStr[i]);
+  // }
+  // return prod - sum;
 
