c008f815982ecb85178b83124589eac2a54cb86d
Zachary <zachjoliver@gmail.com>
2020-05-24 15:12:26 -0700

    Add passing version of number of students doing homework at the same time

--- numStudents.js
+++ numStudents.js
@@ -1,5 +1,9 @@
 var busyStudent = function (startTime, endTime, queryTime) {
-
+  let count = 0;
+  for (let z = 0; z < startTime.length; z++) {
+    if (startTime[z] <= queryTime && endTime[z] >= queryTime) count += 1;
+  }
+  return count;
 };
 
 //run a for loop
