a09c4a5e96257ed0ceffc935babfd77162b47988
Zachary <zachjoliver@gmail.com>
2020-05-25 16:09:52 -0700

    Add function and initial code to balanced string split problem

---
+++ balancedStringSplit.js
@@ -0,0 +1,10 @@
+var balancedStringSplit = function (s) {
+
+};
+
+//create a count obj (resets when close out a substring)
+//iterate through string
+//  add char to count obj
+//  check if obj balanced
+//    if balanced, either increase overall count of substring or add to array of substrings
+//    
