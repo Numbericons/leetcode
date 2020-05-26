cccbc9a10e5fb4d80168d7cba52e6d2aa223a6d4
Zachary <zachjoliver@gmail.com>
2020-05-26 12:46:35 -0700

    Add current version of get target node in cloned tree with new test case and a couple small fixes

--- getTargetCopy.js
+++ getTargetCopy.js
@@ -1,10 +1,10 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+
+// Definition for a binary tree node.
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
 /**
  * @param {TreeNode} original
  * @param {TreeNode} cloned
@@ -24,11 +24,11 @@ var enqueue = function (node, queue) {
   }
 }
 
-var reverseCourse = function(root, tarNode) {
+var reverseCourse = function(tarNode) {
   let directions = [];
 
   while (tarNode.parent) {
-    tarNode.parent.left = tarNode ? directions.unshift('L') : directions.unshift('R');
+    tarNode.parent.left === tarNode ? directions.unshift('L') : directions.unshift('R');
     tarNode = tarNode.parent;
   }
   return directions;
@@ -42,20 +42,53 @@ var getNode = function(node,directions) {
 }
 
 var getTargetCopy = function (original, cloned, target) {
+  if (original === target) return cloned;
   let queue = [original];
   let tarNode;
 
   while (queue.length) {
     const node = queue.shift();
-    if (node.val === target) {
+    if (node === target) {
       tarNode = node;
       break;
     }
     enqueue(node, queue);
   }
-  return getNode(cloned, directions);
+  return getNode(cloned, reverseCourse(tarNode));
 };
 
+// const original = new TreeNode(7);
+// original.left = new TreeNode(4);
+// original.right = new TreeNode(3);
+// original.right.left = new TreeNode(6);
+// original.right.right = new TreeNode(19);
+
+// const clone = new TreeNode(7);
+// clone.left = new TreeNode(4);
+// clone.right = new TreeNode(3);
+// clone.right.left = new TreeNode(6);
+// clone.right.right = new TreeNode(19);
+
+
+// [8, null, 6, null, 5, null, 4, null, 3, null, 2, null, 1]
+
+const original = new TreeNode(8);
+original.right = new TreeNode(6);
+original.right.right = new TreeNode(5);
+original.right.right.right = new TreeNode(4);
+original.right.right.right.right = new TreeNode(3);
+original.right.right.right.right.right = new TreeNode(2);
+original.right.right.right.right.right.right = new TreeNode(1);
+
+const clone = new TreeNode(8);
+clone.right = new TreeNode(6);
+clone.right.right = new TreeNode(5);
+clone.right.right.right = new TreeNode(4);
+clone.right.right.right.right = new TreeNode(3);
+clone.right.right.right.right.right = new TreeNode(2);
+clone.right.right.right.right.right.right = new TreeNode(1);
+
+getTargetCopy(original, clone, original.right)
 
 //To return the node within the cloned tree
 //  as you are going through original tree, 
