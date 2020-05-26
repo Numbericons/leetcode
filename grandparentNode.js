3e09c93297b05c41c9bbed6ecd6729587870d21e
Zachary <zachjoliver@gmail.com>
2020-05-25 18:19:11 -0700

    Add passing version grand parent node

--- grandparentNode.js
+++ grandparentNode.js
@@ -1,3 +1,23 @@
 var sumEvenGrandparent = function (root) {
+  let queue = [root];
+  let sum = 0;
 
+  while (queue.length) {
+    const node = queue.shift();
+    const parent = node.parent;
+    const grandparent = parent ? parent.parent : null;
+    if (grandparent && grandparent.val % 2 === 0) sum += node.val;
+
+    if (node.right) {
+      queue.push(node.right);
+      queue[queue.length - 1].parent = node;
+    }
+    if (node.left) {
+      queue.push(node.left);
+      queue[queue.length - 1].parent = node;
+    }
+  }
+  return sum;
 };
+
+//make a queue
