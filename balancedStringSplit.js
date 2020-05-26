559a3d801ee00775b333fdcdc773a7a038fe9b3f
Zachary <zachjoliver@gmail.com>
2020-05-25 17:49:53 -0700

    Add version passing all specs

--- balancedStringSplit.js
+++ balancedStringSplit.js
@@ -1,5 +1,15 @@
 var balancedStringSplit = function (s) {
+  let count = { R: 0, L: 0 }
+  let total = 0;
 
+  for (let z=0; z<s.length;z++) {
+    count[s[z]] += 1;
+    if (count['R'] === count['L']) {
+      total+=1;
+      count = { R: 0, L: 0 };
+    }
+  }
+  return total;
 };
 
 //create a count obj (resets when close out a substring)
@@ -7,4 +17,4 @@ var balancedStringSplit = function (s) {
 //  add char to count obj
 //  check if obj balanced
 //    if balanced, either increase overall count of substring or add to array of substrings
-//    
+//    reset obj
