c3cc3240624b9d8632704723a72e40543c74ddf8
Zachary <zachjoliver@gmail.com>
2020-05-27 17:09:00 -0700

    Possible bipart update plans

---
+++ possibleBipartition.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} N
+ * @param {number[][]} dislikes
+ * @return {boolean}
+ */
+var possibleBipartition = function (N, dislikes) {
+  let disses = {};
+  
+};
+
+//create an object with keys equal to peoples numbers
+//  values are either those peoples dislikes or the people that dislike them or both
+//    first thought is values are who the key dislikes
+//create group A & B
+//loot through N people and check
+//  if current player 
